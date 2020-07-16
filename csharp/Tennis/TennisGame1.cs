ddfce61bbb170abde4e42078f747134603b56915
Azgarion <arthur.td@hotmail.fr>
2020-07-16 18:12:19 +0200

    redo goldenmaster

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -1,11 +1,13 @@
 using System;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace Tennis
 {
     class TennisGame1 : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
+        private Score m_score1 = new Score();
+        private Score m_score2 = new Score();
         private string player1Name;
         private string player2Name;
 
@@ -19,73 +21,177 @@ namespace Tennis
         {
             if (playerName == "player1")
             {
-                m_score1 += 1;
-                Console.WriteLine(playerName + " won point. Increment score. Score :" + m_score1);
+                if (m_score1 == Point.Forty)
+                {
+                    if(m_score2 < Point.Forty || m_score1.IsAdvantage())
+                    {
+                        m_score1.SetVictory();
+                        Console.WriteLine(playerName + " won point and so is victorious.");
+                    }
+                    else
+                    {
+                        m_score1.SetAdvantage();
+                        Console.WriteLine(playerName + " won point and take advantage.");
+                        m_score2.WithdrawAdvantage();
+                    }
+                }
+                else
+                {
+                    m_score1.AddPoint();
+                    Console.WriteLine(playerName + " won point. Increment score. Score : " + m_score1);
+                }
+
             }
             else
             {
-                m_score2 += 1;
-                Console.WriteLine(playerName + " won point. Increment score. Score :" + m_score2);
+                if (m_score2 == Point.Forty)
+                {
+                    if(m_score1 < Point.Forty || m_score2.IsAdvantage())
+                    {
+                        m_score2.SetVictory();
+                        Console.WriteLine(playerName + " won point and so is victorious.");
+
+                    }
+                    else
+                    {
+                        m_score2.SetAdvantage();
+                        Console.WriteLine(playerName + " won point and take advantage.");
+                        m_score1.WithdrawAdvantage();
+                    }
+                }
+                else
+                {
+                    m_score2.AddPoint();
+                    Console.WriteLine(playerName + " won point. Increment score. Score : " + m_score2);
+                }
+
             }
         }
 
         public string GetScore()
         {
             string score = "";
-            var tempScore = 0;
-            if (m_score1 == m_score2)
+            if (m_score1.IsVictory()) score = "Win for player1";
+            else if (m_score2.IsVictory()) score = "Win for player2";
+            else if (m_score1.IsAdvantage()) score = "Advantage player1";
+            else if (m_score2.IsAdvantage()) score = "Advantage player2";
+            else if (m_score1 == m_score2)
             {
-                switch (m_score1)
+                score = m_score1 + "-All";
+                
+                if (m_score1 == Point.Forty)
                 {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
+                    score = "Deuce";
                 }
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
-            {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
-            }
-            else
-            {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
-            }
+            else score = m_score1.Point + "-" + m_score2.Point;
+            
             Console.WriteLine(score);
             return score;
         }
+    }
+
+    internal class Score
+    {
+        public Point Point { get; private set; }
+
+        private bool _advantage;
+        private bool _victory;
+
+        public Score()
+        {
+            Point = Point.Love;
+        }
+
+        public void AddPoint()
+        {
+            this.Point += 1;
+        }
+
+        public bool IsAdvantage()
+        {
+            return _advantage;
+        }
+
+        public void SetAdvantage()
+        {
+            this._advantage = true;
+        }
+        
+        public void WithdrawAdvantage()
+        {
+            this._advantage = false;
+        }
+        
+        public bool IsVictory()
+        {
+            return _victory;
+        }
+
+        public void SetVictory()
+        {
+            this._victory = true;
+        }
+        
+        public static bool operator ==(Score score1, Score score2)
+        {
+            return score1?.Point == score2?.Point;
+        } 
+        
+        public static bool operator !=(Score score1, Score score2)
+        {
+            if (score1 == null) throw new ArgumentNullException(nameof(score1));
+            if (score2 == null) throw new ArgumentNullException(nameof(score2));
+            return score1.Point != score2.Point;
+        }
+        
+        public static bool operator ==(Score score1, Point score2)
+        {
+            if (score1 == null) throw new ArgumentNullException(nameof(score1));
+            return score1.Point == score2;
+        } 
+        
+        public static bool operator !=(Score score1, Point score2)
+        {
+            if (score1 == null) throw new ArgumentNullException(nameof(score1));
+            return score1.Point != score2;
+        }
+        
+        public static bool operator >=(Score score1, Point score2)
+        {
+            if (score1 == null) throw new ArgumentNullException(nameof(score1));
+            return (int)score1.Point >= (int)score2;
+        }
+        
+        public static bool operator <=(Score score1, Point score2)
+        {
+            if (score1 == null) throw new ArgumentNullException(nameof(score1));
+            return (int)score1.Point <= (int)score2;
+        }
+        
+        public static bool operator >(Score score1, Point score2)
+        {
+            if (score1 == null) throw new ArgumentNullException(nameof(score1));
+            return (int)score1.Point > (int)score2;
+        }
+        
+        public static bool operator <(Score score1, Point score2)
+        {
+            if (score1 == null) throw new ArgumentNullException(nameof(score1));
+            return (int)score1.Point < (int)score2;
+        }
+
+        public override string ToString() => this.Point.ToString();
+    }
+
+    public enum Point
+    {
+        Love = 0,
+        Fifteen = 1,
+        Thirty = 2,
+        Forty = 3,
+        Advantage = 4,
+        Win = 5
     }
 }
 
